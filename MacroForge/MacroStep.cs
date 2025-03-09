923071cdab3e7004d146a8c869ce10d75aadb47d
Sean Leon Maier <sean.maier@outlook.de>
2025-03-09 13:35:13 +0100

    Added class MacroStep.cs

---
+++ MacroForge/MacroStep.cs
@@ -0,0 +1,16 @@
+﻿namespace MacroForge;
+
+public class MacroStep
+{
+    public MacroStep(CommandType commandType, string parameter, int delay)
+    {
+        _commandType = commandType;
+        _parameter = parameter;
+        this.delay = delay;
+    }
+
+    private int _stepNumber;
+    private CommandType _commandType;
+    private string _parameter;
+    private int delay;
+}
