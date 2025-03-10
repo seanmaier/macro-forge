ed86c7845bd0b685aaacd024f8b062b125df7382
Sean Leon Maier <sean.maier@outlook.de>
2025-03-10 13:15:40 +0100

    Added shortcut attribute to MacroData class

--- MacroForge/MacroData.cs
+++ MacroForge/MacroData.cs
@@ -4,13 +4,15 @@ public class MacroData
 {
     private string _name;
     private DateTime _createdAt;
-    private DateTime? _lastChanged = null;
+    private DateTime? _lastChanged;
+    private string? _shortcut;
     private string _description;
     private List<MacroStep> _macroSteps;
 
-    public MacroData(string name, string description, List<MacroStep> macroSteps)
+    public MacroData(string name, string description, List<MacroStep> macroSteps, string? shortcut = null)
     {
         _name = name;
+        _shortcut = shortcut;
         _description = description;
         _macroSteps = macroSteps;
         _createdAt = DateTime.Now;
