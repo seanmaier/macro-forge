3edcaca94cda5cbe3fe31e2cb990612b2a718b4f
Sean Leon Maier <sean.maier@outlook.de>
2025-03-13 11:45:25 +0100

    feature/navigationService -- added MainViewModel.cs

---
+++ MacroForge/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MacroForge.ViewModels;
+
+public class MainViewModel
+{
+    private readonly INavigationService _navigationService;
+    public ICommand NavigationCommand;
+
+    public MainViewModel(INavigationService navigationService)
+    {
+        _navigationService = navigationService;
+        NavigationCommand = new RelayCommand(() => _navigationService.NavigateTo<CreateMacro>());
+    }
+}
