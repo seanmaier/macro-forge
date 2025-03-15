9729f0ee1efbd6f310d72dc26e7fa035bf8e6d23
Sean Leon Maier <sean.maier@outlook.de>
2025-03-15 10:50:31 +0100

    feature/frontend -- Added navigation to edit window

--- MacroForge/ViewModels/MainViewModel.cs
+++ MacroForge/ViewModels/MainViewModel.cs
@@ -9,10 +9,12 @@ public class MainViewModel
 {
     private readonly INavigationService _navigationService;
     public ICommand CreateMacroCommand { get; }
+    public ICommand EditMacroCommand { get; }
 
     public MainViewModel(INavigationService navigationService)
     {
         _navigationService = navigationService;
         CreateMacroCommand = new RelayCommand(() => _navigationService.NavigateTo<CreateMacro>());
+        EditMacroCommand = new RelayCommand(() => _navigationService.NavigateTo<EditMacro>());
     }
 }
