614c95815df4cc45260c3b35b223ba07bd9030d7
Sean Leon Maier <sean.maier@outlook.de>
2025-03-15 10:04:58 +0100

    feature/frontend -- changed Name of NavigationCommand for clarity

--- MacroForge/ViewModels/MainViewModel.cs
+++ MacroForge/ViewModels/MainViewModel.cs
@@ -8,11 +8,11 @@ namespace MacroForge.ViewModels;
 public class MainViewModel
 {
     private readonly INavigationService _navigationService;
-    public ICommand NavigationCommand { get; }
+    public ICommand CreateMacroCommand { get; }
 
     public MainViewModel(INavigationService navigationService)
     {
         _navigationService = navigationService;
-        NavigationCommand = new RelayCommand(() => _navigationService.NavigateTo<CreateMacro>());
+        CreateMacroCommand = new RelayCommand(() => _navigationService.NavigateTo<CreateMacro>());
     }
 }
