779664e1cb8b9bdb81f525c17e302c6407a2ebd8
Sean Leon Maier <sean.maier@outlook.de>
2025-03-20 11:52:53 +0100

    feature/recordMacro -- Added Player class

---
+++ MacroForge/Recorder/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MacroForge.Recorder;
+
+public class Player
+{
+    public void Play(Input[] inputs)
+    {
+        NativeMethods.SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+    }
+
+    public void Cancel()
+    {
+        // TODO implement cancel
+    }
+}
