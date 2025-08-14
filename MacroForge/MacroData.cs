8641343299652f8b50a2926ec22986fef086d1a7
Sean Leon Maier <sean.maier@outlook.de>
2025-08-14 11:45:20 +0200

    refactor: removed verbose getter and setter

--- MacroForge/MacroData.cs
+++ MacroForge/MacroData.cs
@@ -2,19 +2,12 @@
 
 public class MacroData
 {
-    private string _name;
-    private DateTime _createdAt;
-    private DateTime? _lastChanged;
-    private List<string>? _shortcut;
-    private string? _description;
-    private List<MacroStep> _macroSteps;
-
-    public string Name { get => _name; set => _name = value; }
-    public DateTime CreatedAt { get => _createdAt; set => _createdAt = value; }
-    public DateTime? LastChanged { get => _lastChanged; set => _lastChanged = value; }
-    public List<string>? Shortcut { get => _shortcut; set => _shortcut = value; }
-    public string? Description { get => _description; set => _description = value; }
-    public List<MacroStep> MacroSteps { get => _macroSteps; set => _macroSteps = value; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? LastChanged { get; set; }
+    public List<string>? Shortcut { get; set; }
+    public string? Description { get; set; }
+    public List<MacroStep> MacroSteps { get; set; }
 
     public MacroData(string name, string description, List<MacroStep> macroSteps, List<string>? shortcut = null)
     {
--- MacroForge/Models/MacroStep.cs
+++ MacroForge/Models/MacroStep.cs
@@ -2,15 +2,10 @@
 
 public class MacroStep
 {
-    private int _stepNumber;
-    private CommandType _commandType;
-    private string _parameter;
-    private int delay;
-
-    public int StepNumber { get => _stepNumber; set => _stepNumber = value; }
-    public CommandType CommandType { get => _commandType; set => _commandType = value; }
-    public string Parameter { get => _parameter; set => _parameter = value; }
-    public int Delay { get => delay; set => delay = value; }
+    public int StepNumber { get; set; }
+    public CommandType CommandType { get; set; }
+    public string Parameter { get; set; }
+    public int Delay { get; set; }
 
     public MacroStep(CommandType commandType, string parameter, int delay)
     {
