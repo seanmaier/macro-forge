e69a53059a81dace8c24d2052b6bab3f7bd7f3d7
Sean Leon Maier <sean.maier@outlook.de>
2025-08-17 15:26:29 +0200

    feat: added SchemaLoader

---
+++ MacroForge/Schemas/SchemaReader.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace MacroForge.Schemas;
+
+public static class SchemaLoader
+{
+    public static string ReadSchema(string resourceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        using var stream = assembly.GetManifestResourceStream(resourceName)
+                           ?? throw new FileNotFoundException($"Resource {resourceName} not found.");
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+}
